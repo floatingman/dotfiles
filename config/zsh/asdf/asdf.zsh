d6417391b6b446ba6c511093302e16f1760964f9
Daniel Newman <dan@danlovesprogramming.com>
2025-08-18 11:07:55 -0500

    Change conditional for loading asdf

--- config/zsh/asdf/asdf.zsh
+++ config/zsh/asdf/asdf.zsh
@@ -2,7 +2,7 @@
 # vim :set ts=2 sw=2 sts=2 et :
 
 # Initialise asdf-vm
-if [ -e "/usr/bin/asdf" ]; then
+if (( $+commands[asdf] )); then
     export ASDF_DATA_DIR="${HOME}/.asdf"
     export PATH="$ASDF_DATA_DIR/shims:$PATH"
 fi
